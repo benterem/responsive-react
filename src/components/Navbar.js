5227071ee0bdc93352aafc78b653be3aa99215f4
benterem <benterem@gmail.com>
2021-09-29 14:59:54 +0300

    menu items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ const Navbar = () => {
   const [click, setClick] = useState(false)
 
   const handleClick = () => setClick(!click)
+  const closeMobileMenu = () => setClick(!false)
 
   return (
     <>
@@ -17,6 +18,28 @@ const Navbar = () => {
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
           </div>
+          <ul className={click ? 'nav-menu active' : 'nav - menu'}>
+            <li className="nav-time">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-time">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+            <li className="nav-time">
+              <Link to="/products" className="nav-links" onClick={closeMobileMenu}>
+                Products
+              </Link>
+            </li>
+            <li className="nav-time">
+              <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
